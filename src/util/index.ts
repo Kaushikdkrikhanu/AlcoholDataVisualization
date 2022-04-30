40ea0599c670286ce29ada40c1d9dbc2e594a88d
kaushik12dkrikhanu <kaushik12dkrikhanu@gmail.com>
2022-04-30 15:19:02 +0400

    update

--- src/util/index.ts
+++ src/util/index.ts
@@ -9,33 +9,16 @@ export const average = (wines: Wine[]): number[] => {
     //The total malicAcid per class in index 0,1,2 and total count per class in index 3,4,5.
     (prev, curr) => {
       if (curr.class === 1) {
-        return [
-          prev[0] + curr.malicAcid,
-          prev[1],
-          prev[2],
-          prev[3]+1,
-          prev[4],
-          prev[5],
-        ]; 
+        prev[0]+= curr.malicAcid;
+        prev[3]+=1;
       } else if (curr.class === 2) {
-        return [
-          prev[0],
-          prev[1] + curr.malicAcid,
-          prev[2],
-          prev[3],
-          prev[4]+1,
-          prev[5],
-        ];
+        prev[1]+= curr.malicAcid;
+        prev[4]+=1;
       } else {
-        return [
-          prev[0],
-          prev[1],
-          prev[2] + curr.malicAcid,
-          prev[3],
-          prev[4],
-          prev[5]+1,
-        ];
+        prev[2]+= curr.malicAcid;
+        prev[5]+=1;
       }
+      return prev; 
     },
     [0, 0, 0, 0, 0, 0]
   );
