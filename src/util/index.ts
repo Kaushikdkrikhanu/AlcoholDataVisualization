949d8c9700744f5f274ffc1257f6cae1d960985a
kaushik12dkrikhanu <kaushik12dkrikhanu@gmail.com>
2022-04-30 13:39:09 +0400

    update

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,32 +1,48 @@
-import {Wine} from '../interfaces/index'
+import { Wine } from "../interfaces/index";
 
 //return an array with values malic acid content per class
 export const average = (wines: Wine[]): number[] => {
-    let countClass1 = 0;
-    let countClass2 = 0;
-    let countClass3 = 0;
-    
-    const total = wines.reduce(
-      (prev, curr) => {
-        if (curr.class === 1){
-          countClass1++;
-          return [prev[0] + curr.malicAcid, prev[1], prev[2]];          //adding them in an array
-        }              
-        else if (curr.class === 2){
-          countClass2++;
-          return [prev[0], prev[1] + curr.malicAcid, prev[2]];
-        }
-        else {
-          countClass3++;
-          return [prev[0], prev[1], prev[2] + curr.malicAcid];}
-      },
-      [0, 0, 0]
-    );
-    const average = [
-      total[0] / countClass1,
-      total[1] / countClass2,
-      total[2] / countClass3,
-    ];
+  
+  //The total malicAcid per class in index 0,1,2 and total count per class in index 3,4,5.
 
-    return average;
-  };
+  const total = wines.reduce(
+    (prev, curr) => {
+      if (curr.class === 1) {
+        return [
+          prev[0] + curr.malicAcid,
+          prev[1],
+          prev[2],
+          prev[3]+1,
+          prev[4],
+          prev[5],
+        ]; //adding them in an array
+      } else if (curr.class === 2) {
+        return [
+          prev[0],
+          prev[1] + curr.malicAcid,
+          prev[2],
+          prev[3],
+          prev[4]+1,
+          prev[5],
+        ];
+      } else {
+        return [
+          prev[0],
+          prev[1],
+          prev[2] + curr.malicAcid,
+          prev[3],
+          prev[4],
+          prev[5]+1,
+        ];
+      }
+    },
+    [0, 0, 0, 0, 0, 0]
+  );
+  const average = [
+    total[0] / total[3],
+    total[1] / total[4],
+    total[2] / total[5],
+  ];
+
+  return average;
+};
